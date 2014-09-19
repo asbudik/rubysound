8458e962214890afd13fdfb1ba63f7951ac4fffe
Adam Budik <asbudik@live.com>
2014-09-18 18:35:07 -0700

    boilerplate for rubysound final project

---
+++ app.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var db = require("./models/index.js");
+var flash = require("connect-flash");
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());                   // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(morgan('dev'))
+
+app.set("view engine", "ejs");
+
+
+app.get('/api/users', function(req, res) {
+  db.user.findAll({order: [['createdAt', 'DESC']]}).success(function(allUsers) {
+    res.json(allUsers)
+  })
+})
+
+app.post('/api/users', function(req, res) {
+  db.user.create(req.body).success(function(newUser) {
+    res.json(newUser)
+  })
+})
+
+app.get('/api/users/:id', function(req, res) {
+  db.user.find(req.params.id).success(function(foundUser) {
+    foundUser.getSongs().success(function(userSongs) {
+      res.json({user: foundUser, songs: userSongs})
+    })
+  })
+})
+
+app.put('/api/users/:id', function(req, res) {
+  db.user.find(req.params.id).success(function(foundUser) {
+    foundUser.updateAttributes(req.body).success(function() {
+      res.json(foundUser)
+    })
+  })
+})
+
+app.post('/api/users/:id/songs', function(req, res) {
+  db.user.find(req.params.id).success(function(foundUser) {
+    db.song.create(req.body).success(function(newSong) {
+      foundUser.addSong(newSong).success(function() {
+        res.json({user: foundUser, song: newSong})
+      })
+    })
+  })
+})
+
+// app.post('api/songs/:id/votes', function(req, res) {
+//   db.song.find(req.params.id).success(function(foundSong) {
+//     db.vote.create(req.body).success(function(newVote) {
+//       foundSong.addVote(newVote).success(function() {
+//         res.json({song: foundSong, vote: newVote})
+//       })
+//     })
+//   })
+// })
+
+
+app.get('*', function(req, res) {
+  res.render('index'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+app.listen(3000, function() {
+  console.log("SERVER LISTENING ON 3000")
+})
